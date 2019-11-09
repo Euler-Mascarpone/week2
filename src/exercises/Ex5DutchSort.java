7d873bfa922b36f8fd45f9a48bc36e0d434cccbc
Euler-Mascarpone <gusahlquvi@student.gu.se>
2019-11-09 09:12:53 +0100

    Initial commit

---
+++ src/exercises/Ex5DutchSort.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import java.util.Arrays;
+
+import static java.lang.System.out;
+
+public class Ex5DutchSort {
+
+    public static void main(String[] args) {
+        new Ex5DutchSort().program();
+    }
+
+    void program() {
+        int[] arr = {2, 2, 2, 0, 0, 0, 1, 1};
+        //dutchSort(arr);
+        out.println(Arrays.toString(arr)); // [0,0,0,1,1,2,2,2]
+    }
+
+
+    // ----------- Methods ----------------
+
+    // TODO
+}
