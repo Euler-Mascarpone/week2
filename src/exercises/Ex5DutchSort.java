01591b26540dd31289fb307b6e35f1467c51d11b
Euler-Mascarpone <gusahlquvi@student.gu.se>
2019-11-12 12:51:59 +0100

    Completed exercises 5-6

--- src/exercises/Ex5DutchSort.java
+++ src/exercises/Ex5DutchSort.java
@@ -11,8 +11,8 @@ public class Ex5DutchSort {
     }
 
     void program() {
-        int[] arr = {2, 2, 2, 0, 0, 0, 1, 1};
-        //dutchSort(arr);
+        int[] arr = {0,1,0,1,2,1,1,1,0,1,2, 2, 2, 0, 0, 0, 1, 1};
+        dutchSort(arr);
         out.println(Arrays.toString(arr)); // [0,0,0,1,1,2,2,2]
     }
 
@@ -20,4 +20,60 @@ public class Ex5DutchSort {
     // ----------- Methods ----------------
 
     // TODO
+
+
+    void dutchSort(int[] arr) {
+        int highCount = arr.length - 1; //Index for highest index we don't know is a two
+        int lowCount = 0;
+        int[] count = {lowCount,highCount};
+        int i = 0;
+        while (arr[i] == 0) { //runs past the zero elements that doesn't need to be sorted
+            i++;
+        }
+        count[0] = i; //Index for highest index we don't know is zero
+        while (i <= count[1]) { //runs through the rest of the code.
+            if (arr[i] == 0) {
+                leftSwitcher(arr, i, count);//Do stuff
+            } else if (arr[i] == 2) {
+                rightSwitcher(arr, i, count);//Do other stuff
+            } else {
+                //Nothing, ones are left as they are.
+            }
+            i++;
+        }
+    }
+
+    void leftSwitcher(int[] arr, int i, int[] count) {
+        if (i == count[0]) {
+            count[0]=count[0] + 1; //Everything already sorted
+        } else { //Not sorted, element at lowCount index must be a 1,
+            arr[count[0]] = 0;
+            arr[i] = 1; //Switching
+            count[0]=count[0] + 1;
+        }
+    }
+
+    void rightSwitcher(int[] arr, int i, int[] count) {
+
+        int k = arr[count[1]];
+        while (k == 2) {
+            if (i == count[1]) {
+                //Then we are done
+                break;
+            } else {
+                k = arr[count[1] - 1];
+            }
+            count[1] = count[1] - 1;
+        }
+        //Here k != 2
+        if (k == 0){
+            leftSwitcher(arr,i,count); //Send zero to the left
+        } else { //Then k == 1
+            arr[i] = 1;
+        }
+        arr[count[1]] = 2;
+        count[1] = count[1] - 1;
+    }
+
 }
+
--- src/exercises/Ex6ThreeSumToZero.java
+++ src/exercises/Ex6ThreeSumToZero.java
@@ -22,11 +22,33 @@ public class Ex6ThreeSumToZero {
         int[] arr3 = {-2, 1, 1};
         int[] arr4 = {0, 0, 0, 0};
 
-        //out.println(Arrays.toString(getThreeSum(arr1)));    // [1, 4, 6]
-        //out.println(Arrays.toString(getThreeSum(arr2)));    // []
-        //out.println(Arrays.toString(getThreeSum(arr3)));    // [0, 1, 2]
-        //out.println(Arrays.toString(getThreeSum(arr4)));    // [ 0, 1 ,3 ] or any
+        out.println(Arrays.toString(getThreeSum(arr1)));    // [1, 4, 6]
+        out.println(Arrays.toString(getThreeSum(arr2)));    // []
+        out.println(Arrays.toString(getThreeSum(arr3)));    // [0, 1, 2]
+        out.println(Arrays.toString(getThreeSum(arr4)));    // [ 0, 1 ,3 ] or any
     }
 
+    int[] getThreeSum(int[] arr) {
+        int k;
+        for (int i = 0; i < arr.length; i++) {
+            k = arr[i];
+            int low = i + 1;
+            int high = arr.length - 1;
+            while(low < high){
+                if(k + arr[low] + arr[high] < 0){ //Then we need to increase low,
+                    //Even if high could be increased, since it starts at length - 1 we have already checked all possibilites above the current high
+                    //Since Array is sorted
+                    low++;
+                } else if(k + arr[low] + arr[high] > 0){ //Then we need to decrease high
+                    high--;
+                } else {
+                    int[] index = {i,low,high};
+                    return index;
+                }
+            }
+        }
+        int[] index = {};
+        return index;
+    }
 
 }
